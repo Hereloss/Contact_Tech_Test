d1b3b0dcc910c9437e363291dff68a0d167ea965
Chris Thomas <hereloss@googlemail.com>
2022-02-01 17:12:10 +0000

    Test passing, first test refactored to fit find_people test

--- lib/location_filter.rb
+++ lib/location_filter.rb
@@ -2,12 +2,18 @@ require 'json'
 
 class LocationFilter
 
-  def see_data
+  attr_reader :data_hash
+
+  def initialize
     file = File.read('./example_data.json')
-    data_hash = JSON.parse(file)
+    @data_hash = JSON.parse(file)
   end
 
   def find_people(location)
-
+    @data_hash.each do |data|
+      if data["location"] == location
+        return data["name"]
+      end
+    end
   end
 end
--- spec/location_filer_spec.rb
+++ spec/location_filer_spec.rb
@@ -2,8 +2,8 @@ require './lib/location_filter'
 
 describe LocationFilter do
 
-  it 'see_data method displays the parsed Json' do
-    expect(subject.see_data[0]).to eq({"name"=>"Homer Simpson", "location"=>"Springfield", "date_of_birth"=>"1956-05-12"})
+  it 'The data_hash variable displays the parsed Json' do
+    expect(subject.data_hash[0]).to eq({"name"=>"Homer Simpson", "location"=>"Springfield", "date_of_birth"=>"1956-05-12"})
   end
 
   it 'Location filter has a method to find the data which takes in an argument' do
