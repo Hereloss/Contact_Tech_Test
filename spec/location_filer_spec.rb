e89e2948fec25b67af30e640aebdf4331643152f
Chris Thomas <hereloss@googlemail.com>
2022-02-01 17:05:07 +0000

    Test for method added

--- spec/location_filer_spec.rb
+++ spec/location_filer_spec.rb
@@ -6,4 +6,8 @@ describe LocationFilter do
     expect(subject.see_data[0]).to eq({"name"=>"Homer Simpson", "location"=>"Springfield", "date_of_birth"=>"1956-05-12"})
   end
 
+  it 'Location filter has a method to find the data which takes in an argument' do
+    expect(subject).to respond_to(:find_people)
+  end
+
 end
