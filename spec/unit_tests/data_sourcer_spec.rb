03d59c0703c4a9b0a11b08f59bfa664bf2a54ca3
Chris Thomas <hereloss@googlemail.com>
2022-02-02 15:06:18 +0000

    Tests to manage malformed JSON data added

--- spec/unit_tests/data_sourcer_spec.rb
+++ spec/unit_tests/data_sourcer_spec.rb
@@ -7,9 +7,18 @@ describe DataSourcer do
                                          'name' => 'Maggie Simpson' })
   end
 
-  it 'will raise and error if an invalid path is chosen' do
+  it 'will raise an error if an invalid path is chosen' do
     expect do
       subject.amend_source_location('./example_data_3.json')
     end.to raise_error('No such file or directory @ rb_sysopen - ./example_data_3.json')
+  end
+
+  it 'will raise an error if the chosen JSON file is invalid' do
+    expect do
+      subject.amend_source_location('./malformed_data.json')
+    end.to raise_error('Error - JSON data is malformed')
+    expect do
+      subject.amend_source_location('./malformed_data_2.json')
+    end.to raise_error('Error - JSON data is malformed')
   end
 end
--- spec/unit_tests/printer_spec.rb
+++ spec/unit_tests/printer_spec.rb
@@ -5,4 +5,14 @@ describe Printer do
     input = [{ 'name' => 'Homer Simpson' }, { 'name' => 'Marge Simpson' }]
     expect(Printer.print_formatted_output(input)).to eq("Homer Simpson\nMarge Simpson")
   end
+
+  it 'will raise an error if the data is malformed to have 2 name keys' do
+    input = [{ 'name' => 'Homer Simpson', 'name' => 'Marge Simpson'}]
+    expect { Printer.print_formatted_output(input) }.to raise_error('Error in data - 2 name keys for one entry')
+  end
+
+  it 'will return "1 person with no name" if a entry has the location but no name' do
+    input = [{ 'location' => 'Springfield' }]
+    expect(Printer.print_formatted_output(input)).to eq("1 person with no name")
+  end
 end
