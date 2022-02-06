7506a6f2b85a2a5dd9e5ff54adada60bef3871af
Chris Thomas <hereloss@googlemail.com>
2022-02-06 19:16:19 +0000

    Created JSON duplicate key checking gem and installed and added to project, old class file removed

--- Gemfile
+++ Gemfile
@@ -11,3 +11,5 @@ gem 'rubocop', '~> 1.25'
 gem 'simplecov-console', '~> 0.9.1'
 
 gem 'json', '~> 2.6'
+
+gem "json_duplikey_stopper", "~> 0.0.0"
--- lib/data_sourcer.rb
+++ lib/data_sourcer.rb
@@ -1,4 +1,4 @@
-require_relative 'duplicate_checking'
+require 'json_duplikey_stopper'
 
 # This class loads the data from a data source. This data source can be changed with amend_source_location
 class DataSourcer
@@ -6,12 +6,12 @@ class DataSourcer
 
   def initialize
     file = File.read('./example_data.json')
-    @data_hash = JSON.parse(file, { object_class: DuplicateChecking })
+    @data_hash = JSON.parse(file, { object_class: JsonDuplikeyStopper })
   end
 
   def amend_source_location(path)
     file = File.read(path)
-    @data_hash = JSON.parse(file, { object_class: DuplicateChecking })
+    @data_hash = JSON.parse(file, { object_class: JsonDuplikeyStopper })
   rescue StandardError
     raise 'Error - JSON data is malformed or location does not contain JSON file'
   end
